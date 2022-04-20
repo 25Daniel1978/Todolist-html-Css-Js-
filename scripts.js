1d29de3f54b5da44e86dea5374c521c0fa9d15e7
25Daniel1978 <biedensteindaniel6@gmail.com>
2022-04-20 16:33:28 +0200

    Update: NewTodo() function, glass morph, center div

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function NewTodo(){
+    // console.log("Hello Class");
+    var listItem = document.createElement("li");
+    var inputField = document.getElementById("myInput").value;
+    var tNode = document.createTextNode(inputField);
+
+    listItem.appendChild(tNode);
+
+    document.getElementById("myUL").appendChild(listItem);
+}
